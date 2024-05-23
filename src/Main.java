e3885971c2298f23bd1a4c61fbb95afc5ea00b48
POO <jonathanrm445@gmail.com>
2024-05-23 09:05:50 -0500

    Intento de comparticion

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Carpeta compartida ....");
-        System.out.println("Funcionando....");
+        System.out.println("Funcionando--");
     }
 }
