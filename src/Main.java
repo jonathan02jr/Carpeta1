342d1416e23aef06ae7f5a60a465420fc905d8be
POO <jonathanrm445@gmail.com>
2024-05-23 09:09:31 -0500

    cambio de genero

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args){
 
-        Pelicula movie = new Pelicula("Avengers","Ciencia Ficcion",2012);
+        Pelicula movie = new Pelicula("Avengers","Super herores",2012);
 
         movie.mostrar();
     }
